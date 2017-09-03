f7b7c043a3c03c3245367c3ce958d532703546b3
Nadezhda Kutsenko <nadikutsenko@gmail.com>
2017-09-03 18:12:03 +0200

    Add rating icon

--- app/booksOverview/booksOverview.js
+++ app/booksOverview/booksOverview.js
@@ -13,7 +13,29 @@ angular.module('myApp.booksOverview', ['ngRoute'])
   API.getEvents().then(function (data) {
     console.log(data);
     $scope.lastModified = data.data.last_modified.substr(0, 10);
-    $scope.books = data.data.results;
-    
+    // $scope.books = data.data.results;
+    $scope.books = [];
+    for (var i = 0; i < data.data.results.length; i++) {
+      var book = {
+        title: data.data.results[i].book_details[0].title,
+        author: data.data.results[i].book_details[0].author,
+        rank: data.data.results[i].rank,
+        rank_last_week: data.data.results[i].rank_last_week,
+      };
+      if (book.rank == book.rank_last_week) {
+        book.up = false;
+        book.down = false;
+        book.same = true;
+      } else if (book.rank > book.rank_last_week) {
+        book.up = true;
+        book.down = false;
+        book.same = false;
+      } else {
+        book.up = false;
+        book.down = true;
+        book.same = false;
+      }
+      $scope.books.push(book);
+    }
   });
 }]);
